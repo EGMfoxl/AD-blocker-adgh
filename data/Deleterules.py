625efa36351ba861149f1c478e67f5cdc929c3a1
EGMfoxl <121678224+EGMfoxl@users.noreply.github.com>
2025-01-01 20:44:20 +0800

    Update Deleterules.py

--- data/Deleterules.py
+++ data/Deleterules.py
@@ -1,9 +1,10 @@
-# 文件路径
+# 规则文件路径
 file_path = './rules.txt'
 
-# 读取、过滤、写回文件
+# 读取并处理规则文件
 with open(file_path, 'r', encoding='utf-8') as f:
-    rules = [line.strip() for line in f if line.strip() and not line.startswith('@@') and not line.startswith('!')]
+    rules = {line.strip() for line in f if line.strip() and not line.startswith(('!', '@@'))}
 
+# 排序规则并写回文件
 with open(file_path, 'w', encoding='utf-8') as f:
     f.write("\n".join(sorted(rules)))
